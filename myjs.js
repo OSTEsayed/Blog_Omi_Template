099f64331632d83d25246eb64d6640c817072508
oudjani sayed taki eddine <97703724+OSTEsayed@users.noreply.github.com>
2022-04-14 02:18:46 +0200

    nav and slider

---
+++ myjs.js
@@ -0,0 +1,29 @@
+
+let Index = 1;
+let first = document.getElementsByClassName("souslider");
+
+function goslide(n) {
+    Slide(Index += n);
+}
+function Slide(n) {
+  let i;
+  if (n > first.length) {Index = 1}    
+  if (n < 1) {Index = first.length}
+  for (i = 0; i < first.length; i++) {
+    first[i].style.display = "none";  
+  }
+  first[Index-1].style.display = "block";
+}
+function AutoSlides() {
+    let i;
+    for (i = 0; i < first.length; i++) {
+      first[i].style.display = "none";
+    }
+    Index++;
+    if (Index > first.length) {Index = 1}
+    first[Index-1].style.display = "flex";
+    setTimeout(AutoSlides, 4000); 
+}
+AutoSlides();
+
+Slide(Index)
