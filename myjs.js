0c2ce4daca0db5c257b65b2bcfade6fdc1a856c0
oudjani sayed taki eddine <97703724+OSTEsayed@users.noreply.github.com>
2022-04-27 04:54:09 +0200

    contac

--- myjs.js
+++ myjs.js
@@ -30,3 +30,33 @@ function AutoSlides() {
 AutoSlides();
 
 Slide(Index)
+
+var ser= document.getElementsByClassName("Are")[0];
+var serv= document.getElementsByClassName("serv");
+var what= document.getElementsByClassName("what")[0];
+
+var observer = new IntersectionObserver(function(entries) {
+	// isIntersecting is true when element and viewport are overlapping
+	// isIntersecting is false when element and viewport don't overlap
+	if(entries[0].isIntersecting === true){
+
+    console.log('Element has just become visible in screen');
+    function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  async function demo() {
+    what.classList ="what whatanim";
+    what.style.display="block";
+      for (let i = 0; i < serv.length; i++) {
+          serv[i].classList ="serv whatanim";
+          await sleep(1000);
+      }
+      console.log('Done');
+  }
+  
+  demo();
+  }
+}, { threshold: [0.5] });
+
+observer.observe(ser);
